b212d0619d262d760607ff7ae79ce5131b1a1f1a
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-03-24 17:56:20 +0100

    vector: get gpu pointer

--- spinta/vector.cuh
+++ spinta/vector.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 namespace spinta{
 namespace sequential{
@@ -11,6 +12,7 @@ namespace parallel{
     template <typename T>
     class vector {
         T *device_ptr = nullptr;
+        T *host_ptr = nullptr;
         std::size_t size;
 
         public:
@@ -28,6 +30,15 @@ namespace parallel{
 
             std::cout << "spinta vector distructor" << std::endl;
         }
+
+        T* gpu_ptr(){
+            return this->device_ptr;
+        }
+
+        std::vector<T> std_vector(){
+            std::cout << "TODO: implement std_vector" << std::endl;
+        }
+
     };
 }; // parallel
 }; // spinta
--- testing/main_vector.cu
+++ testing/main_vector.cu
@@ -3,7 +3,19 @@
 #include <vector>
 #include <iostream>
 
+template<typename T>
+__global__ void setvalue(T* src, T* dest){
+    int i = threadIdx.x;
+
+    dest[i] = src[i];
+}
+
 int main(){
     // Testing RAII
     spinta::parallel::vector<int> myvet(10);
+    spinta::parallel::vector<int> dest(10);
+
+    setvalue<int><<<1,1>>>(myvet.gpu_ptr(), dest.gpu_ptr());
+
+    cudaDeviceSynchronize();
 }
